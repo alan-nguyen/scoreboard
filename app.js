ef124fdaddfeffaf7f10d6365a5763f959a52a8c
Alan Nguyen <tuan8101@gmail.com>
2021-01-06 16:47:50 -0700

    Create props for Player

--- app.js
+++ app.js
@@ -8,21 +8,21 @@ function Header(props) {
   );
 }
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className="player">
-      <span className="player-name">Alan</span>
+      <span className="player-name"> {props.name} </span>
 
-      <Counter />
+      <Counter score={props.score} />
     </div>
   );
 };
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
+      <span className="counter-score">{props.score}</span>
       <button className="counter-action increment"> + </button>
     </div>
   );
@@ -34,7 +34,7 @@ const App = () => {
       <Header title="Scoreboard" totalPlayers={1} />
 
       {/* Players list */}
-      <Player />
+      <Player name="Alan" score={50} />
     </div>
   );
 };
