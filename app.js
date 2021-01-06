3017460baf2b60286b96fb317e413a99655c1484
Alan Nguyen <tuan8101@gmail.com>
2021-01-06 15:41:34 -0700

    Add props to Header

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-function Header() {
+function Header(props) {
+  console.log(props);
   return (
     <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players: 1</span>
+      <h1>{props.title}</h1>
+      <span className="stats">Players: {props.totalPlayers}</span>
     </header>
   );
 }
@@ -30,7 +31,7 @@ const Counter = () => {
 const App = () => {
   return (
     <div className="scoreboard">
-      <Header />
+      <Header title="Scoreboard" totalPlayers={1} />
 
       {/* Players list */}
       <Player />
