40dd46442557b9b96ffa886a20732a593e41c274
Alan Nguyen <tuan8101@gmail.com>
2021-01-06 15:08:31 -0700

    Create Player

--- app.js
+++ app.js
@@ -7,4 +7,18 @@ function Header() {
   );
 }
 
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">Alan</span>
+
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">35</span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    </div>
+  );
+};
+
 ReactDOM.render(<Header />, document.getElementById('root'));
