8e63dd62ee6a25d0f9cba43e551f008dac4ab33e
Alan Nguyen <tuan8101@gmail.com>
2021-01-06 22:44:20 -0700

    Create incrementScore fun

--- app.js
+++ app.js
@@ -35,18 +35,34 @@ const Player = (props) => {
     <div className="player">
       <span className="player-name"> {props.name} </span>
 
-      <Counter score={props.score} />
+      <Counter />
     </div>
   );
 };
 
 class Counter extends React.Component {
+  state = {
+    score: 0,
+  };
+
+  incrementScore = () => {
+    this.setState({
+      score: this.state.score + 1,
+    });
+  };
+
   render() {
     return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{this.props.score}</span>
-        <button className="counter-action increment"> + </button>
+        <span className="counter-score">{this.state.score}</span>
+        <button
+          className="counter-action increment"
+          onClick={this.incrementScore}
+        >
+          {' '}
+          +{' '}
+        </button>
       </div>
     );
   }
@@ -59,11 +75,7 @@ const App = (props) => {
 
       {/* Players list */}
       {props.initialPlayers.map((player) => (
-        <Player
-          name={player.name}
-          score={player.score}
-          key={player.id.toString()}
-        />
+        <Player name={player.name} key={player.id.toString()} />
       ))}
     </div>
   );
