277469f008d942c5191b05072631e9bad01e03b3
Alan Nguyen <tuan8101@gmail.com>
2021-01-07 13:16:28 -0700

    Refractor Counter to function

---
+++ src/components/Counter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// Stateless component, just takes in props
+const Counter = (props) => {
+  // incrementScore = () => {
+  //   this.setState((prevState) => ({
+  //     score: prevState.score + 1,
+  //   }));
+  // };
+
+  // decrementScore = () => {
+  //   this.setState((prevState) => ({
+  //     score: prevState.score - 1,
+  //   }));
+  // };
+
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">{props.score}</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
+
+export default Counter;
