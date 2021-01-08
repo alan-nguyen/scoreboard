c3c13426d8bc90691dc958b23d9e9ae772e45756
Alan Nguyen <tuan8101@gmail.com>
2021-01-07 17:12:30 -0700

    Create handleScoreChange fun

--- src/components/App.js
+++ src/components/App.js
@@ -28,6 +28,25 @@ class App extends Component {
     ],
   };
 
+  handleScoreChange = (index, delta) => {
+    this.setState((prevState) => {
+      // New 'players' array – a copy of the previous `players` state
+      const updatedPlayers = [...prevState.players];
+      // A copy of the player object we're targeting
+      const updatedPlayer = { ...updatedPlayers[index] };
+
+      // Update the target player's score
+      updatedPlayer.score += delta;
+      // Update the 'players' array with the target player's latest score
+      updatedPlayers[index] = updatedPlayer;
+
+      // Update the `players` state without mutating the original state
+      return {
+        players: updatedPlayers,
+      };
+    });
+  };
+
   handleRemovePlayer = (id) => {
     this.setState((prevState) => {
       return {
@@ -42,12 +61,14 @@ class App extends Component {
         <Header title="Scoreboard" totalPlayers={this.state.players.length} />
 
         {/* Players list */}
-        {this.state.players.map((player) => (
+        {this.state.players.map((player, index) => (
           <Player
             name={player.name}
             score={player.score}
             id={player.id}
             key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange}
             removePlayer={this.handleRemovePlayer}
           />
         ))}
